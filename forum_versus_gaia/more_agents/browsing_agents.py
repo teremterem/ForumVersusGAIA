f29c216ec883f005c01ef45ed8767133f8849801
Oleksandr Tereshchenko <toporok@gmail.com>
2023-12-30 21:05:50 +0200

    preparing for retries upon exceptions

--- forum_versus_gaia/more_agents/browsing_agents.py
+++ forum_versus_gaia/more_agents/browsing_agents.py
@@ -21,11 +21,11 @@ for a given user query. The user is looking for a PDF document. Your job is to e
 is the most likely to contain the PDF document the user is looking for.\
 """
 
-EXTRACT_URL_PROMPT = """\
-Your name is {AGENT_ALIAS}. You will be provided with a SerpAPI JSON response that contains a list of search results \
-for a given user query. The user is looking for an answer to the ORIGINAL QUESTION (you will se it below). Your job \
-is to extract a URL that, in your opinion, is the most likely to contain the answer the user is looking for.\
-"""
+# EXTRACT_URL_PROMPT = """\
+# Your name is {AGENT_ALIAS}. You will be provided with a SerpAPI JSON response that contains a list of search results \
+# for a given user query. The user is looking for an answer to the ORIGINAL QUESTION (you will se it below). Your job \
+# is to extract a URL that, in your opinion, is the most likely to contain the answer the user is looking for.\
+# """
 
 EXTRACT_PDF_URL_FROM_PAGE_PROMPT = """\
 Your name is {AGENT_ALIAS}. You will be provided with the content of a web page that was found via web search with a \
@@ -95,55 +95,13 @@ async def pdf_finder_agent(ctx: InteractionContext, recursion: int = 5) -> None:
     page_url = (await fast_gpt_completion(prompt=prompt).amaterialize_content()).strip()
     assert_valid_url(page_url)
 
-    ctx.respond(
-        pdf_finder_agent.quick_call(
-            page_url,
-            # TODO Oleksandr: make it possible to pass `branch_from` to `quick_call` and `call` directly
-            # TODO Oleksandr: `branch_from` should accept either a message promise or a concrete message or a message id
-            #  or even a message sequence (but not your own list of messages ?)
-            conversation=ConversationTracker(
-                forum, branch_from=await ctx.request_messages.aget_concluding_msg_promise()
-            ),
-            recursion=recursion - 1,
-        )
-    )
-
-
-@forum.agent
-async def browsing_agent(ctx: InteractionContext, original_question: str = "") -> None:
-    """
-    Using a search engine finds and returns from the internet a web page that satisfies a search query. Input should
-    be a search query.
-    """
-    query = await ctx.request_messages.amaterialize_concluding_content()
-    query = query.strip()
-
-    organic_results = get_serpapi_results(query)
-
-    prompt = [
-        {
-            "content": EXTRACT_URL_PROMPT.format(AGENT_ALIAS=ctx.this_agent.alias),
-            "role": "system",
-        },
-        {
-            "content": (
-                f"USER QUERY: {query}\n\nTHE ORIGINAL QUESTION THIS QUERY WAS DERIVED FROM: {original_question}"
-            ),
-            "role": "user",
-        },
-        {
-            "content": f"SERPAPI SEARCH RESULTS: {json.dumps(organic_results)}",
-            "role": "user",
-        },
-        {
-            "content": "PLEASE ONLY RETURN A URL AND NO OTHER TEXT.\n\nURL:",
-            "role": "system",
-        },
-    ]
-    page_url = (await fast_gpt_completion(prompt=prompt).amaterialize_content()).strip()
-    assert_valid_url(page_url)
-
-    async with get_httpx_client() as httpx_client:
-        httpx_response = await httpx_client.get(page_url)
-
-    ctx.respond(httpx_response.text)
+    response_msgs = await pdf_finder_agent.quick_call(
+        page_url,
+        # TODO Oleksandr: make it possible to pass `branch_from` to `quick_call` and `call` directly
+        # TODO Oleksandr: `branch_from` should accept either a message promise or a concrete message or a message
+        #  id or even a message sequence (but not your own list of messages ?)
+        conversation=ConversationTracker(forum, branch_from=await ctx.request_messages.aget_concluding_msg_promise()),
+        recursion=recursion - 1,
+    ).amaterialize_as_list()
+    # TODO Oleksandr: amaterialize_as_list is needed to catch exceptions here and not later - what to do about it ?
+    ctx.respond(response_msgs)
