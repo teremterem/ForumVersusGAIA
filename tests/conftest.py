800da8a9eeaa116bc9d5144f93fb3e129a3fdf4c
Oleksandr Tereshchenko <toporok@gmail.com>
2023-12-31 21:25:42 +0200

    minor

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""
-This module usually contains fixtures that are used in tests.
-"""
-import warnings
-
-# TODO Oleksandr: get rid of this warning suppression when PromptLayer doesn't produce "Expected Choice but got dict"
-#  warning anymore
-warnings.filterwarnings("ignore", module="pydantic")
