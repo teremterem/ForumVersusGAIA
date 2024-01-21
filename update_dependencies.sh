b17ad81ab8a26221e4ceb0055d0c11e8a6ef1365
Oleksandr Tereshchenko <toporok@gmail.com>
2024-01-21 11:49:16 +0200

    try out AgentForum v0.0.6

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -1,3 +1,3 @@
 pip-compile --upgrade --strip-extras
 pip-compile --upgrade --strip-extras dev-requirements.in
-pip-sync requirements.txt dev-requirements.txt editable-requirements.txt
+./sync_dependencies.sh
